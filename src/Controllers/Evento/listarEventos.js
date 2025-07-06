9663d10d867aa027ab4dbfdcb4d338b59fd9e5ae
Robert-Cortez-Rudi <robercortezrudi@gmail.com>
2025-07-05 21:54:09 -0300

    Controller de listagem de eventos

---
+++ src/Controllers/Evento/listarEventos.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const listarEventos = async (req, res) => {
+  let where = {}
+
+  if (req.user.tipo_usuario === 'MODERADOR') {
+    where.resposta_da_moderacao = 'pendente'
+  } else {
+
+    where.resposta_da_moderacao = 'aprovado'
+    where.aprovado_pela_modereacao = true
+
+  }
+
+  const eventos = await prisma.evento.findMany({
+    where,
+    include: { usuario: true }
+  })
+  res.json(eventos)
+}
